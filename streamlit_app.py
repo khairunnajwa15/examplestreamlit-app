51c20f47980400e9a12198885dc4d3bb5c05ed4b
khairunnajwa15 <khairunnajwa1576@gmail.com>
2026-08-12 14:58:11 +0800

    Add feedback feature and selectbox to app

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.title("🎈 SAYA KHAIRUN NAJWA")
 st.write(
     " F1042 SAYA BERJAYA"
+    
 )
 st.feedback("stars")
 choice = st.selectbox("pick one", ["DIT5A","DIT5B","DIT5C","DIT5D"])
